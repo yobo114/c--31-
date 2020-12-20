ad9a28046a31d32ef90f06be7c49cb74f48d0439
yobo114 <jacob.aaron@sunnyvaleisd.net>
2020-12-19 20:11:25 -0600

    stage6 completed

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var gamestate = "onSling";
 
 
 function preload() {
@@ -69,16 +70,18 @@ function draw(){
 }
 
 function mouseDragged(){
+    if(gamestate!=="launched")
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gamestate = "launched"
 }
 
 function keyPressed(){
     if(keyCode === 32){
-        slingshot.attach(bird.body);
+        // slingshot.attach(bird.body);
     }
 }
